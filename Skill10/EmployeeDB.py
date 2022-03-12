ed9d4053ba40e117007355294043c79a13555165
2000030172 <96280332+2000030172@users.noreply.github.com>
2022-03-12 13:47:54 +0530

    Delete Skill10 directory

--- Skill10/EmployeeDB.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-con=sqlite3.connect("feedback.db")
-print("DataBase Opened Successfully..!")
-con.execute("create table Feedback(id INTEGER PRIMARY KEY AUTOINCREMENT,email TEXT UNIQUE NOT NULL, feedback TEXT "
-            "UNIQUE NOT NULL)")
-print("Table created Successfully")
-con.close()
